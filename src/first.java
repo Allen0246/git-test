caf45e8d3f91efd2d6315f97ec6e57f50bcdd7d6
Kelemen Richárd otthoni laptop <kelemen.richard1@gmail.com>
2022-05-02 18:32:56 +0200

    push

--- src/first.java
+++ src/first.java
@@ -1,9 +1,12 @@
 public class first {
     public static void main(String[] args) {
-        osszeadas(3,5);
+        first lgp= new first();
+        int osszeadas = lgp.osszeadas("3","5");
         System.out.println("valami");
     }
-    public static int osszeadas(int a, int b){
-        return a+ b;
+    private static int osszeadas(String a, String b){
+        int szam1= Integer.parseInt(a);
+        int szam2= Integer.parseInt(b);
+        return szam1+szam2;
     }
 }
