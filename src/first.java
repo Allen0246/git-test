273713b287fcd108ad7faecab6108e8d88b2abf7
Kelemen Richárd otthoni laptop <kelemen.richard1@gmail.com>
2022-05-02 18:11:18 +0200

    valami

--- src/first.java
+++ src/first.java
@@ -1,6 +1,7 @@
 public class first {
     public static void main(String[] args) {
         osszeadas(3,5);
+        System.out.println("valami");
     }
     public static int osszeadas(int a, int b){
         return a+ b;
